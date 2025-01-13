5a9ff508d3bf29513ee5d42f86ab9510f8ef8a48
Sabrina Mukai Nishidate <saahmukai.ads.16@gmail.com>
2025-01-13 16:26:15 -0300

    Commit primeira atv

---
+++ PrimeiraAtv.py
@@ -0,0 +1,89 @@
+print ("Olá Mundo!")
+print ("Olá Python!") 
+print ("Me chamo Sabrina Mukai")
+
+print("A pequenina aranha escalou a tromba d'água.")
+print("Caiu a chuva e lavou a aranha.")
+
+print("A pequenina aranha escalou a tromba d'água.")
+print ()
+print("Caiu a chuva e lavou a aranha.")
+
+print("A pequenina aranha \nescalou a tromba d'água.")
+print ()
+print("Caiu a chuva \ne lavou a aranha.")
+
+print("A aranha pequeninha", "subiu", "a tromba d'agua.")
+
+print ("Meu nome é", "Sabrina")
+print("Sabrina Mukai")
+
+print ("Meu nome é", "Sabrina. ", end=" ")
+print("Sabrina Mukai")
+
+print ("Meu nome é", end=" ")
+print("Sabrina Mukai")
+
+print ("meu", "nome", "é", "Sabrina", "Mukai", sep="-")
+
+print ("meu", "nome", "é", sep="_", end="*")
+print ("Sabrina", "Mukai", sep="*", end="*\n")
+print ("Nishidate")
+
+print("Sabrina"*2)
+
+print("Sa\nbri\nna")
+
+print("versão original:")
+###################
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+###################
+print("com menos 'print()' invocações:")
+###################
+print("    *\n   * *\n  *   *\n *     *\n***   ***")
+print("  *   *\n  *   *\n  *****")
+###################
+print("mais alto:")
+###################
+print("        *")
+print("       * *")
+print("      *   *")
+print("     *     *")
+print("    *       *")
+print("   *         *")
+print("  *           *")
+print(" *             *")
+print("******     ******")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *******")
+###################
+print("dobrou:")
+###################
+print("        *        "*2)
+print("       * *       "*2)
+print("      *   *      "*2)
+print("     *     *     "*2)
+print("    *       *    "*2)
+print("   *         *   "*2)
+print("  *           *  "*2)
+print(" *             * "*2)
+print("******     ******"*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *******     "*2)
