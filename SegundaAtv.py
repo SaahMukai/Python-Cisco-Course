cafe7865cf45d740aa64092043674313ec9751a5
Sabrina Mukai Nishidate <saahmukai.ads.16@gmail.com>
2025-01-16 10:21:00 -0300

    Segundo commit py.udemy

---
+++ SegundaAtv.py
@@ -0,0 +1,10 @@
+# Literais: Um literal são dados cujos valores são determinados pelo próprio literal.
+
+print("2")
+print (2)
+
+####uma string, que você já conhece,
+####e um número inteiro, algo completamente novo.
+####inteiros x floats(flutuantes) --> contem parte fracionária
+
+print(0o123)
