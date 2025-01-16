cfebda64ac1d4e1a780bb3406e57688fe66c7cd2
Sabrina Mukai Nishidate <saahmukai.ads.16@gmail.com>
2025-01-16 14:08:08 -0300

    Commit de ATT Segunda.atv

--- SegundaAtv.py
+++ SegundaAtv.py
@@ -7,4 +7,44 @@ print (2)
 ####e um número inteiro, algo completamente novo.
 ####inteiros x floats(flutuantes) --> contem parte fracionária
 
+
+#### Conversão direta do octal e hexa
 print(0o123)
+print(0x123)
+
+
+#### Floats (Função decimal não vazia)
+print(.4)
+print(4.)
+print(2.5)
+print(-0.4)
+
+
+#### Int. / Float
+print(300000000)
+print(3E8) # = 3x10^8
+
+
+#### String
+print("I\'m Sabrina Mukai")
+print("I'm sabrina mUKAI")
+
+
+#### Valores Booleanos
+print(True>False)
+print(True<False)
+
+
+### Exercicios
+#1
+print("Eu sou")
+print("\"aprendizado\"")
+print("\"\"Python\"\"")
+
+#2
+#Qual o valor de 1011? 11
+#Quais são os Literais em Python?
+# Inteiros
+# Floats
+# Strings
+# Booleanos
