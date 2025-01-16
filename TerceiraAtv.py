26557411d4b2d54611bb2487083489a57fc70f96
Sabrina Mukai Nishidate <saahmukai.ads.16@gmail.com>
2025-01-16 15:05:47 -0300

    Terceira.atv adição de imagem prioridade

---
+++ TerceiraAtv.py
@@ -0,0 +1,90 @@
+# Python como calculadora
+
+print(2+2)
+ 
+
+## Operadores básicos
+### Um operador é um símbolo da linguagem de programação, capaz de operar com os valores.
+### Por exemplo, assim como na aritmética, o sinal de + (mais) é o operador que é capaz de adicionar dois números, dando o resultado da adição.
+# +
+# -
+# *
+# /
+# //
+# %
+# **
+
+
+#### Exponenciação
+print(2 ** 3)
+print(2 ** 3.)
+print(2. ** 3)
+print(2. ** 3.)
+
+
+#### Multiplicação
+print(2 * 3)
+print(2 * 3.)
+print(2. * 3)
+print(2. * 3.)
+
+
+#### Divisão
+print(6 / 3)
+print(6 / 3.)
+print(6. / 3)
+print(6. / 3.)
+
+
+#### Divisão de número inteiro (divisão arredondada)
+print(6 // 3)
+print(6 // 3.)
+print(6. // 3)
+print(6. // 3.)
+
+
+#### Restante (módulo)
+print(14 % 4) # --> % (percentual)
+# 14 // 4 dá 3 → este é o quociente inteiro;
+# 3 * 4 dá 12 → como resultado da multiplicação de quociente e divisor;
+# 14 - 12 dá 2 → este é o restante.
+
+print(12 % 4.5)
+# 3.0 – não 3 mas 3.0. A regra ainda funciona:
+# 12 // 4.5 dá 2.0,
+# 2.0 * 4.5 dá 9.0,
+# 12 - 9.0 dá 3.0.
+
+
+#### Adição
+print(-4 + 4)
+print(-4. + 8)
+
+
+#### O operador de subtração, os operadores unários e binários
+print(-4 - 4)
+print(4. - 8)
+print(-1.1)
+print(+2)
+
+
+#### Operadores e suas prioridades
+print(2 + 3 * 5)
+
+
+#### Operadores e suas ligações
+print(9 % 6 % 2)
+print(6 % 2 % 9)
+
+# --> Agr com exponenciação
+print(2 ** 2 ** 3)
+
+print(-3 ** 2)
+print(-2 ** 3)
+print(-(3 ** 2))
+ 
+
+######## Lista de prioridades ######
+
+
+
